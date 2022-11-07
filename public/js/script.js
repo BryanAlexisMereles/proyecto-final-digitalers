499cc222c485d83f935a5d1664ee2af52f0d9ca8
Bryan Alexis Mereles <bryanmereles@gmail.com>
2022-11-07 14:49:38 -0300

    Agregados archivos

---
+++ public/js/script.js
@@ -0,0 +1,25 @@
+particlesJS.load('particles-js', './assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+
+let hiddenChat = document.getElementById("hidden-chat")
+let chatIcon = document.getElementById("chat-icon")
+
+hiddenChat.addEventListener('click',() => {
+  if (chatIcon.className == "esconder-logo-chat")
+  {
+    chatIcon.removeAttribute("class");
+    chatIcon.setAttribute("class","mostrar-logo-chat");
+    console.log("Salgo");
+    hiddenChat.setAttribute('data-content', '>')
+  }
+  else{
+    chatIcon.setAttribute("class","esconder-logo-chat")
+    console.log("Entro");
+    hiddenChat.setAttribute('data-content', '<')
+  }
+  
+})
+
+hiddenChat.addEventListener("animationend", ()=>{}, false);
